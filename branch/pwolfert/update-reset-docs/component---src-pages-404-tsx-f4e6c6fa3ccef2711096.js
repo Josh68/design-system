254257429119664ee94cb290893207c3dac45e88
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-06-06 17:52:12 -0700

    Updates

---
+++ branch/pwolfert/update-reset-docs/component---src-pages-404-tsx-f4e6c6fa3ccef2711096.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_cmsgov_cms_design_system_docs=self.webpackChunk_cmsgov_cms_design_system_docs||[]).push([[218],{3503:function(t,s,e){e.r(s);var c=e(8668),n=e(2959),a=e(8902),o=e(2322);s.default=function(t){var s=t.data,e=t.location,m=(0,n.Z)();return(0,o.jsx)(c.Z,{frontmatter:s.mdx.frontmatter,location:e,theme:m,children:(0,o.jsx)(a.Z,{data:s.mdx.body,theme:m})})}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-f4e6c6fa3ccef2711096.js.map
