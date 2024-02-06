79062ad9051d50e74838657a7f36b64ef100d3cd
Sarah Sachs <zarahzachz@users.noreply.github.com>
2024-02-06 16:19:47 -0500

    Updates

---
+++ branch/WNMGDS-2160/consolidate-docs-theme/component---src-pages-contact-tsx-38d57df042fc038d9f85.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_cmsgov_cms_design_system_docs=self.webpackChunk_cmsgov_cms_design_system_docs||[]).push([[650],{7187:function(t,s,e){e.r(s);var n=e(6624),a=e(2959),o=e(6671),d=e(2322);s.default=function(t){var s,e=t.data,m=t.location,c=e.mdx.slug,l=(0,a.Z)();return(0,d.jsx)(n.Z,{frontmatter:e.mdx.frontmatter,location:m,slug:c,theme:l,tableOfContentsData:null===(s=e.mdx.tableOfContents)||void 0===s?void 0:s.items,children:(0,d.jsx)(o.Z,{data:e.mdx.body,theme:l})})}}}]);
+//# sourceMappingURL=component---src-pages-contact-tsx-38d57df042fc038d9f85.js.map
